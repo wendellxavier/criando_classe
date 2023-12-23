47c8da8ff28f540b2d19cb20d3ab4a4b12a8c2c5
wendell <wendell_xw20@hotmail.com>
2023-12-23 10:10:49 -0300

    poo

---
+++ main.py
@@ -0,0 +1,14 @@
+from main_test import Pessoa
+
+p1 = Pessoa('wendell', 27)
+p2 = Pessoa('joão', 22)
+
+
+print(p1.puxar_nascimento())
+
+
+p1.falar('python')
+p2.falar('filme')
+p1.comer('maçã')
+
+
